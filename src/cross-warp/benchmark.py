551e30f50386b3f4f9c7b2aaa769032eb655b8e7
Pablo Raigoza <raigozapablo@gmail.com>
2025-11-22 20:16:08 -0500

    added requirements.txt

--- src/cross-warp/benchmark.py
+++ src/cross-warp/benchmark.py
@@ -1,8 +1,6 @@
 import argparse
 import os
 
-launcher_path = "launcher.cu"
-
 def compute_max_args(max_shared_mem, fix_num_pairs):
     possible_pairs = [fix_num_pairs] if fix_num_pairs >= 1 else [1 << i for i in range(0, 6)]  # 1,2,4,8,16,32
     possible_msg_sizes = [1 << i for i in range(0, 20)]  # 1B to 1MiB
@@ -16,7 +14,6 @@ def compute_max_args(max_shared_mem, fix_num_pairs):
                 valid_configs.append((num_pairs, msg_size, shared_mem_per_block))
 
     return valid_configs
-    
 
 if __name__ == "__main__":
     # we need to pass in max shared memory per block
