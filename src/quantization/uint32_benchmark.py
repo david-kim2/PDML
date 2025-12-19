0980f6e5f60cf35f517d572552228971b77f514a
Sayeem2004 <MohammadSSKC@gmail.com>
2025-12-19 12:14:50 -0500

    add quantization code

---
+++ src/quantization/uint32_benchmark.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+
+
+def compute_max_args(min_mem, max_mem, fix_num_pairs):
+    possible_pairs = [fix_num_pairs] if fix_num_pairs >= 1 else [1 << i for i in range(0, 11)]  # 1 - 1024
+    possible_msg_sizes = [1 << i for i in range(0, 40)]  # 1B to 1TiB
+
+    valid_configs = []
+    for num_pairs in possible_pairs:
+        for msg_size in possible_msg_sizes:
+            global_mem_size = 2 * msg_size + 2 * num_pairs
+            if global_mem_size >= min_mem and global_mem_size <= max_mem and msg_size % num_pairs == 0:
+                valid_configs.append((num_pairs, msg_size, global_mem_size))
+
+    return valid_configs
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--min-mem", type=int, default=0, help="Minimum global memory in MiB")
+    parser.add_argument("--max-mem", type=int, required=True, help="Maximum global memory in MiB")
+    parser.add_argument("--fix-num-pairs", type=int, default=1, help="Fix number of thread pairs to this value")
+    parser.add_argument("--num-runs", type=int, default=10, help="Number of runs per configuration")
+    args = parser.parse_args()
+
+    valid_configs = compute_max_args(args.min_mem * 1024 * 1024, args.max_mem * 1024 * 1024, args.fix_num_pairs)
+    for num_pairs, msg_size, mem in valid_configs:
+        cmd = f"./bin_main_uint32 {msg_size} {num_pairs} {args.num_runs}"
+        print(f"Running with num_pairs={num_pairs}, msg_size={msg_size}, mem={mem} bytes")
+        os.system(cmd)
+        print("============================================")
---
+++ src/quantization/uint64_kernel.cu
@@ -0,0 +1,132 @@
+#include <cooperative_groups.h>
+#include "../common.hpp"
+#include <stdint.h>
+#include <cstdio>
+#include <cuda.h>
+
+
+namespace cg = cooperative_groups;
+
+
+__device__ __forceinline__ uint64_t get_timestamp() {
+    uint64_t ret;
+    asm volatile("mov.u64 %0, %globaltimer;" : "=l"(ret));
+    return ret;
+}
+
+
+__global__ void uint64_cross_block_echo_kernel(
+    uint64_t* metrics, // Place to store output metrics, shape [n_runs][num_pairs][8]
+    uint64_t* client_buf, // Global memory buffer for client-to-server messages
+    uint64_t* server_buf, // Global memory buffer for server-to-client messages
+    volatile uint8_t* finished_c2s, // Global memory flags for client-to-server completion
+    volatile uint8_t* finished_s2c, // Global memory flags for server-to-client completion
+    size_t msg_size, // Message size in bytes, split evenly between pairs
+    int num_pairs, // Number of client-server pairs (threads per side)
+    int n_runs // Number of runs to perform to allow averaging
+) {
+    // Thread variables
+    cg::grid_group grid    = cg::this_grid();
+    int bid                = blockIdx.x;
+    int tid                = threadIdx.x;
+    size_t msg_size_thread = msg_size / num_pairs;
+    size_t msg_size_word   = (msg_size_thread + 7) / 8;
+
+    // Global memory layout, size = num_pairs * msg_size_word + num_pairs
+    uint64_t* client_offset = client_buf + tid * msg_size_word;
+    uint64_t* server_offset = server_buf + tid * msg_size_word;
+
+    // Iterating over different runs
+    for (int run = 0; run < n_runs; run++) {
+        int output_idx = (run * num_pairs + tid) * 8;
+
+        if (bid == 0 && tid < num_pairs) { // CLIENT
+            // Reset client-to-server buffer to zero
+            for (size_t i = 0; i < msg_size_word; i++)
+                client_offset[i] = 0;
+            finished_c2s[tid] = 0;
+            __threadfence();
+        } else if (bid == 1 && tid < num_pairs) { // SERVER
+            // Reset server-to-client buffer to zero
+            for (size_t i = 0; i < msg_size_word; i++)
+                server_offset[i] = 0;
+            finished_s2c[tid] = 0;
+            __threadfence();
+        }
+        grid.sync();
+
+        uint64_t client_start, client_end, client_recv_start, client_recv_end;
+        uint64_t server_recv_start, server_recv_end, server_start, server_end;
+        uint64_t fill = uint64_t(tid + 1);
+
+        if (bid == 0 && tid < num_pairs) { // CLIENT
+            // Begin client-to-server communication
+            client_start = get_timestamp();
+            finished_c2s[tid] = 1;
+            __threadfence();
+
+            for (size_t i = 0; i < msg_size_word; i++)
+                client_offset[i] = fill;
+            __threadfence();
+
+            finished_c2s[tid] = 2;
+            __threadfence();
+            client_end = get_timestamp();
+        } else if (bid == 1 && tid < num_pairs) { // SERVER
+             // Wait for client response
+            while (finished_c2s[tid] == 0);
+            server_recv_start = get_timestamp();
+            while (finished_c2s[tid] != 2);
+            server_recv_end = get_timestamp();
+        }
+        grid.sync();
+
+        if (bid == 0 && tid < num_pairs) { // CLIENT
+            // Wait for server response
+            while (finished_s2c[tid] == 0);
+            client_recv_start = get_timestamp();
+            while (finished_s2c[tid] != 2);
+            client_recv_end = get_timestamp();
+
+            // Confirm data integrity
+            bool valid = true;
+            for (size_t i = 0; i < msg_size_word; i++)
+                valid &= (server_offset[i] == fill);
+
+            // Store client metrics
+            if (!valid) {
+                metrics[output_idx + 0] = 0ull;
+                metrics[output_idx + 1] = 0ull;
+                metrics[output_idx + 2] = 0ull;
+                metrics[output_idx + 3] = 0ull;
+            } else {
+                metrics[output_idx + 0] = client_start;
+                metrics[output_idx + 1] = client_end;
+                metrics[output_idx + 2] = client_recv_start;
+                metrics[output_idx + 3] = client_recv_end;
+            }
+            __threadfence();
+        } else if (bid == 1 && tid < num_pairs) { // SERVER
+            // Begin client-to-server communication
+            server_start = get_timestamp();
+            finished_s2c[tid] = 1;
+            __threadfence();
+
+            for (size_t i = 0; i < msg_size_word; i++)
+                server_offset[i] = client_offset[i];
+            __threadfence();
+
+            finished_s2c[tid] = 2;
+            __threadfence();
+            server_end = get_timestamp();
+
+            // Store server metrics
+            metrics[output_idx + 4] = server_recv_start;
+            metrics[output_idx + 5] = server_recv_end;
+            metrics[output_idx + 6] = server_start;
+            metrics[output_idx + 7] = server_end;
+            __threadfence();
+        }
+        grid.sync();
+    }
+}
